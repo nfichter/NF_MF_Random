8a9996533e8b587b87f0f0413648efd76702f8e9
nfichter <nfichter@stuy.edu>
2016-10-26 21:09:30 -0400

    not working

--- rand.c
+++ rand.c
@@ -23,6 +23,7 @@ int main() {
   for (i = 0; i < 10; i++) {
     printf("Integer #%d: %d\n",i+1,*p[i]);
   }
+
   int fd2 = open("randnums",O_CREAT | O_WRONLY,0644);
   if (fd2 == -1) {
     printf("%d - %s",errno,strerror(errno));
@@ -30,6 +31,23 @@ int main() {
   }
   void *vp = p;
   int numwritten = write(fd2,vp,10*sizeof(int));
+
   close(fd);
   close(fd2);
+  //added stuff vvv
+  fd2 = open("randnums",O_RDONLY);
+  if (fd2 == -1) {
+    printf("%d - %s",errno,strerror(errno));
+    return 0;
+  }
+  
+  int *p2[10];
+  for (i = 0; i < 10; i++) {
+    p2[i] = (int*)malloc(10*sizeof(int));
+    read(fd2,p2[i],sizeof(int));
+  }
+  for (i = 0; i < 10; i++) {
+    printf("Integer #%d (2nd round): %d\n",i+1,*p2[i]);
+  }
+  
 }
